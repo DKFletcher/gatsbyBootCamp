40d47a5e39ce9b7fbba380ccbf1ea63e02937f09
Donald Fletcher <dfletcher@whatmotivateslearning.com>
2020-05-22 16:08:38 +0100

    dynamicaly generated list on blog page using graphql.

--- markdownSite/src/pages/blog.js
+++ markdownSite/src/pages/blog.js
@@ -1,11 +1,41 @@
 import React from "react"
 import Layout from "../components/layout"
+import { graphql, useStaticQuery } from "gatsby"
 
 const BlogPage = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      allMarkdownRemark {
+        edges {
+          node {
+            frontmatter {
+              title
+              date
+            }
+          }
+        }
+      }
+    }
+  `)
+  const posts = data.allMarkdownRemark.edges.map(post => (
+    <li>
+      <h3>{post.node.frontmatter.title}</h3>
+      <h3>{post.node.frontmatter.date}</h3>
+    </li>
+  ))
   return (
     <Layout>
       <h2>My Blog</h2>
-      <p>Posts will show up here later on.</p>
+      <ol>
+        {data.allMarkdownRemark.edges.map((edge) => {
+          return (
+            <li>
+              <h3>{edge.node.frontmatter.title}</h3>
+              <p>{edge.node.frontmatter.date}</p>
+            </li>
+          )
+        })}
+      </ol>
     </Layout>
   )
 }
---
+++ markdownSite/src/posts/gatsby.md
@@ -0,0 +1,12 @@
+---
+title: 'The Great Gatsby Boot Camp'
+date: '2020-05-23'
+---
+
+ I just experienced my first bootcamp!
+
+ ## The Topics Covered:
+
+ 1. Gatsby
+ 2. React
+ 3. GraphQL
